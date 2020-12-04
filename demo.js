4ee6be8711804e302ab9c90df84a7b66fc7cde7e
Geert <geert@geertvos.net>
2020-12-04 14:56:28 +0100

    First commit, adding demo

---
+++ demo.js
@@ -0,0 +1,15 @@
+var init = function() {
+    print("Loaded demo plugin");
+}
+
+var run = function() {
+    
+    
+    list = k8s.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+    for (i=0; i< list.getItems().size(); i++) {
+      var item = list.getItems().get(i);
+      print(item.getMetadata().getName());
+    }
+    
+    slack.send("#dev-monitoring","Hi this is Automator! You now have "+list.getItems().size()+" pods running.");
+}
