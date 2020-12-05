59cbd668e0a40bcca8032654ffb7c2631c5afad8
Geert Vos <info@geertvos.net>
2020-12-05 20:46:12 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,15 +1,16 @@
 var init = function() {
-    log.info("Loaded demo plugin");
+    log.info("Loaded demo plugin.");
 }
 
 var run = function() {
-    
-    
     list = k8s.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
     for (i=0; i< list.getItems().size(); i++) {
       var item = list.getItems().get(i);
       log.info(item.getMetadata().getName());
     }
-    
     slack.send("#dev-monitoring","Hi this is Automator! You now have "+list.getItems().size()+" pods running. Test Update!");
+}
+
+var destroy = function() {
+    log.info("Unloaded demo plugin.");
 }
