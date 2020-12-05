db00b990b09480335baa8d0d7d7436076bb121a2
Geert Vos <info@geertvos.net>
2020-12-05 20:35:29 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -8,7 +8,7 @@ var run = function() {
     list = k8s.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
     for (i=0; i< list.getItems().size(); i++) {
       var item = list.getItems().get(i);
-      print(item.getMetadata().getName());
+      log.info(item.getMetadata().getName());
     }
     
     slack.send("#dev-monitoring","Hi this is Automator! You now have "+list.getItems().size()+" pods running. Test Update!");
