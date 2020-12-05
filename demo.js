4ffa2b9aa026564b2fddf799d0a038c770886b74
Geert Vos <info@geertvos.net>
2020-12-05 20:27:56 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -11,5 +11,5 @@ var run = function() {
       print(item.getMetadata().getName());
     }
     
-    slack.send("#dev-monitoring","Hi this is Automator! You now have "+list.getItems().size()+" pods running.");
+    slack.send("#dev-monitoring","Hi this is Automator! You now have "+list.getItems().size()+" pods running. Test Update!");
 }
