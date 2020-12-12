68bb7493405196cb35b449d71e7f386b0a28b4f8
Geert Vos <info@geertvos.net>
2020-12-12 16:03:16 +0100

    Update events-demo.js

--- events-demo.js
+++ events-demo.js
@@ -1,12 +1,12 @@
+var onReceiveEvent = function(event) {
+    log.info("Demo received event: "+event.getMessage());
+}
+
 var init = function() {
     plugins.load("log");
     plugins.load("eventbus");
     log.info("Loaded event demo.");
-    eventbus.registerListener(this, "scripts.executed");
-}
-
-var onReceiveEvent = function(event) {
-    log.info("Demo received event: "+event.getMessage());
+    eventbus.registerListener(onReceiveEvent, "scripts.executed");
 }
 
 var run = function() {
