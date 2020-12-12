518551d5fc184a50acf8e889fc35ea3a10456313
Geert Vos <info@geertvos.net>
2020-12-12 15:50:40 +0100

    Update events-demo.js

--- events-demo.js
+++ events-demo.js
@@ -10,21 +10,7 @@ var onReceiveEvent = function(event) {
 }
 
 var run = function() {
-    list = k8s.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
-    for (i=0; i< list.getItems().size(); i++) {
-      var item = list.getItems().get(i);
-      log.info(item.getMetadata().getName());
-    }
-    slack.send("#dev-monitoring","Hi this is Automator! You now have "+list.getItems().size()+" pods running.");
-    
-    nodes = rabbitmq.getNodes();
-    for(i=0; i < nodes.size(); i++) {
-        var node = nodes.get(i);
-        log.info("RabbitMQ Node: "+node.getName()+" memory used: "+node.getMemoryUsed());
-        slack.send("#dev-monitoring","Your RabbitMQ node "+node.getName()+" is using this much memory: "+node.getMemoryUsed());
-    }
 }
 
 var destroy = function() {
-    slack.send("#dev-monitoring","Goodbye. I am unloaded and will no longer run on your cluster.");
 }
