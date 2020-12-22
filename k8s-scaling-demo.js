fd40571af6bd456e6718c402b8a0806d7498494d
Geert <geert@geertvos.net>
2020-12-22 18:51:39 +0100

    Whitespace

--- k8s-scaling-demo.js
+++ k8s-scaling-demo.js
@@ -10,12 +10,12 @@ var run = function() {
     
     deployments = k8s.apps().deployments().withLabel("automator/min-replicas").list();
     count = deployments.getItems().size();
-    for(i=0;i<count;i++) {
+    for( i=0 ; i < count ; i++ ) {
       var deployment = deployments.getItems().get(i);
       var name = deployment.getMetadata().getName();
       var replica = deployment.getSpec().getReplicas();
       var expected = parseInt(deployment.getMetadata().getLabels().get("automator/min-replicas"));
-      if(replica < expected) {
+      if( replica < expected ) {
         log.info("Bumping up replica for "+name+" from "+replica+" to "+expected);
         k8s.apps().deployments().inNamespace("default").withName(name).scale(expected);
       }
